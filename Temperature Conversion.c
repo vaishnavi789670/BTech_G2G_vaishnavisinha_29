c10c4626d6c2b2d54f8b591d85810e75e5358000
vaishnavi789670 <25116cn179@glbitm.ac.in>
2026-02-22 10:08:20 +0530

    Add files via upload

---
+++ Temperature Conversion.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{   float c,f;
+    printf("Enter the temperature in Centigrade","%f");
+    scanf("%f",&c);
+    f=(9*c)/5+32;
+    printf("Temperature in Fahrenheit is %f",f);
+}
